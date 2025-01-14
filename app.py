53972a2f5ca11af7611dd2747e7915418105e886
arielo <109183577+SugarForTheP1ll@users.noreply.github.com>
2025-01-14 01:01:35 -0300

    Pré-Alterações
    Pré-Alterações antes da conexão com a API do Disqus

--- app.py
+++ app.py
@@ -22,6 +22,17 @@ def login():
 def sucesso():
     return render_template('home.html')
 
+@app.route('/vidadigital')
+def vida_digital():
+    return render_template('vidadigital.html')
+
+@app.route('/videojogos')
+def video_jogos():
+    return render_template('videojogos.html')
+
+@app.route('/pirata')
+def pirataria():
+    return render_template('pirataria.html')
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
@@ -43,6 +54,7 @@ def cadastro():
 @app.route('/comunidades')
 def tec():
     return render_template('comunidades.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
