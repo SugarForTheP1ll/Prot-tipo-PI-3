6a95880d38fa2276a8ec6bf7070de124cfe5e4d0
arielo <109183577+SugarForTheP1ll@users.noreply.github.com>
2025-01-14 21:34:46 -0300

    Projeto final
    fim

--- app.py
+++ app.py
@@ -5,21 +5,8 @@ app.secret_key = 'PROTPISECRET_KEY'
 
 
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['senha']
-        if email == "usuario@exemplo.com" and senha == "senha123":
-            return redirect(url_for('sucesso'))
-        else:
-            flash('Credenciais inválidas. Tente novamente.')
-    
-    return render_template('login.html')
-
-
-@app.route('/home')
-def sucesso():
+@app.route('/')
+def home():
     return render_template('home.html')
 
 @app.route('/vidadigital')
@@ -34,22 +21,6 @@ def video_jogos():
 def pirataria():
     return render_template('pirataria.html')
 
-@app.route('/cadastro', methods=['GET', 'POST'])
-def cadastro():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        email = request.form['email']
-        senha = request.form['senha']
-        confirma_senha = request.form['confirma_senha']
-
-        if senha != confirma_senha:
-            flash('As senhas não coincidem!')
-            return redirect(url_for('cadastro'))
-
-        flash('Cadastro realizado com sucesso!')
-        return redirect(url_for('login'))  
-
-    return render_template('cadastro.html')
 
 @app.route('/comunidades')
 def tec():
